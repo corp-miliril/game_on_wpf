79c87d994c1c496f273c347aa84ae261a9f2babd
Milirik <56610764+Milirik@users.noreply.github.com>
2020-08-09 15:34:36 +0300

    start project

---
+++ LightHeroes3/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace LightHeroes3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+
+        public MainWindow()
+        {
+            timer.Tick += new EventHandler(Engine);
+            timer.Interval = TimeSpan.FromMilliseconds(20);
+            timer.Start();
+            InitializeComponent();
+        }
+
+        private void GameArea_KeyDown(object sender, KeyEventArgs e)
+        {
+           
+        }
+
+        private void GameArea_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+        public void Engine(object sender, EventArgs e) {
+            Tank t1 = new Tank();
+            GameArea.Children.Add(t1.Shape);
+            Canvas.SetLeft(t1.Shape, 200);
+        }
+    }
+}
---
+++ LightHeroes3/Units.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LightHeroes3
+{
+    abstract class Unit
+    {
+        public int damage;
+        public int hp;
+        public int defence;
+        //public int coords;
+        public int cost;
+        public ImageBrush Skin = new ImageBrush();
+        public Rectangle Shape = new Rectangle();
+
+        abstract public void Attack(Unit u);
+    }
+
+   
+
+    class Soldier: Unit
+    {
+        public Soldier() {
+            this.damage = 1;
+            this.hp = 10;
+            this.defence = 10;
+            this.cost = 10;
+            this.Skin.ImageSource = new BitmapImage(new Uri("Images/soldier.jpg", UriKind.Relative));
+            this.Shape.Fill = this.Skin;
+        }
+
+        public override void Attack(Unit u)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    class Tank : Unit
+    {
+        public Tank()
+        {
+            this.damage = 1;
+            this.hp = 10;
+            this.defence = 10;
+            this.cost = 10;
+            this.Skin.ImageSource = new BitmapImage(new Uri("Images/tank.jpg", UriKind.Relative));
+            this.Shape.Fill = this.Skin;
+        }
+
+        public override void Attack(Unit u)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    class Archer : Unit
+    {
+        public Archer()
+        {
+            this.damage = 1;
+            this.hp = 10;
+            this.defence = 10;
+            this.cost = 10;
+            this.Skin.ImageSource = new BitmapImage(new Uri("Images/archer.jpg", UriKind.Relative));
+            this.Shape.Fill = this.Skin;
+        }
+
+        public override void Attack(Unit u)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    class Archangel : Unit
+    {
+        public Archangel()
+        {
+            this.damage = 1;
+            this.hp = 10;
+            this.defence = 10;
+            this.cost = 10;
+            this.Skin.ImageSource = new BitmapImage(new Uri("Images/archangel.jpg", UriKind.Relative));
+            this.Shape.Fill = this.Skin;
+        }
+
+        public override void Attack(Unit u)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
